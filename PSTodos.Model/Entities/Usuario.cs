213c26f73788ebc7c3e3639bb4c29a05d587e650
Lucas Duarte <lucasduarte599@gmail.com>
2017-02-11 00:56:44 -0200

    Adds field 'senha' to usuario

--- PSTodos.Model/Entities/Usuario.cs
+++ PSTodos.Model/Entities/Usuario.cs
@@ -7,6 +7,7 @@ namespace PSTodos.Model.Entities
         public string Login { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public string Senha { get; set; }
         public bool Ativo { get; set; }
         public DateTime DtInclusao { get; set; }
     }

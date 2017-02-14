a044e6f10fc74c86d1e0e182c7218af691fbff88
Lucas Duarte <lucasduarte599@gmail.com>
2017-02-13 23:57:38 -0200

    Creates UsuarioPerfil Rest service

---
+++ PSTodos.Mvc/RESTServices/UsuarioPerfilRESTService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using PSTodos.Mvc.Results;
+using PSTodos.Mvc.ViewModels;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSTodos.Mvc.RESTServices
+{
+    public class UsuarioPerfilRESTService : BaseRESTService
+    {
+        private readonly string usuarioPerfilUri = apiUri + "/usuarios/{0}/perfis/{1}";
+
+        public async Task<GenericResult<UsuarioPerfilViewModel>> CadastrarUsuarioPerfilAsync(int usuarioId, int perfilId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.PostAsync(string.Format(usuarioPerfilUri, usuarioId, perfilId), new StringContent("", Encoding.UTF8, "application/json"));
+                return JsonConvert.DeserializeObject<GenericResult<UsuarioPerfilViewModel>>(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        public async Task<GenericResult> RemoverUsuarioPerfilAsync(int usuarioId, int perfilId)
+        {
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.DeleteAsync(string.Format(usuarioPerfilUri, usuarioId, perfilId));
+                return JsonConvert.DeserializeObject<GenericResult<PerfilViewModel>>(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
---
+++ PSTodos.Mvc/ViewModels/UsuarioPerfilViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSTodos.Mvc.ViewModels
+{
+    public class UsuarioPerfilViewModel
+    {
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+        public int PerfilId { get; set; }
+        public virtual Perfil Perfil { get; set; }
+        public bool Ativo { get; set; }
+    }
+}

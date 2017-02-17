ea8babcde2405355b72851b10110efcf59a2c509
lucasduarte <lucasduarte599@gmail.com>
2017-02-17 11:51:58 -0200

    Fixes simpleInjector

---
+++ PSTodos.Mvc/App_Start/SimpleInjectorConfig.cs
@@ -0,0 +1,25 @@
+﻿using PSTodos.RESTServices;
+using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Web.Mvc;
+
+namespace PSTodos.Mvc
+{
+    public static class SimpleInjectorConfig
+    {
+        public static void Config()
+        {
+            // 1. Create a new Simple Injector container
+            var container = new Container();
+
+            // 2. Configure the container (register)
+            container.Register<IPerfilRESTService, PerfilRESTService>();
+            container.Register<IUsuarioPerfilRESTService, UsuarioPerfilRESTService>();
+            container.Register<IUsuarioRESTService, UsuarioRESTService>();
+
+
+            // 4. Register the container as MVC IDependencyResolver.
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    }
+}
--- PSTodos.Mvc/Global.asax.cs
+++ PSTodos.Mvc/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using PSTodos.RESTServices;
-using SimpleInjector;
-using SimpleInjector.Integration.Web.Mvc;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -16,17 +13,7 @@ namespace PSTodos.Mvc
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            // 1. Create a new Simple Injector container
-            var container = new Container();
-
-            // 2. Configure the container (register)
-            container.Register<IPerfilRESTService, PerfilRESTService>();
-            container.Register<IUsuarioPerfilRESTService, UsuarioPerfilRESTService>();
-            container.Register<IUsuarioRESTService, UsuarioRESTService>();
-            
-
-            // 4. Register the container as MVC3 IDependencyResolver.
-            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+            SimpleInjectorConfig.Config();
         }
 
     }
